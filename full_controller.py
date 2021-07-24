d1b8b92bfb109c354e824c950b9722d649b0a17a
Diogo Miguel Simões <46751892+diogomgsimoes@users.noreply.github.com>
2021-07-24 20:10:30 +0100

    Update full_controller.py

--- full_controller.py
+++ full_controller.py
@@ -22,7 +22,6 @@ import json
 _switches = []
 mymac = {}
 
-# adjacency map [sw1][sw2]->port from sw1 to sw2
 adjacency = defaultdict(lambda: defaultdict(lambda: None))
 datapath_list = {}
 byte = defaultdict(lambda: defaultdict(lambda: None))
@@ -32,35 +31,44 @@ bw_available = defaultdict(lambda: defaultdict(lambda: None))
 bw = defaultdict(lambda: defaultdict(lambda: None))
 costs = defaultdict(lambda: defaultdict(lambda: None))
 n_flows = defaultdict(lambda: defaultdict(lambda: None))
+paths = {}
+active_paths = {}
+last_active_paths_len = 0
+ended_flows = 0
+p_dict = {}
+requested_flows = 0
 
-target_srcmac = "00:00:00:00:00:01"
-target_dstmac = "00:00:00:00:00:03"
 
-
-def get_path_dynamic(src, dst, first_port, final_port):
-    global bw_available
+def get_path_dynamic(src, dst, first_port, final_port, src_ip, dst_ip):
+    # nested dict to add the calculated paths
+    global paths
+    # save the destination
     dest = dst
+
     _switches_keys = set(_switches)
-    
-    print("Searching for path between %s and %s" % (src, dst))
-    
     distance = {}
     previous = {}
 
+    # for each switch set max distance and no previous hop
     for dpid in _switches_keys:
         distance[dpid] = float('Inf')
         previous[dpid] = None
 
+    # set src to 0 distance
     distance[src] = 0
 
     while len(_switches_keys) > 0:
+        # closest neighbour
         closest_node = minimum_distance(distance, _switches_keys)
+        # choose it by removing from the set
         _switches_keys.remove(closest_node)
         
         for node in _switches_keys:
+            # if there is a port connecting the nodes (next neighbour)
             if adjacency[closest_node][node] != None:
-                #w = costs[str(closest_node)][str(node)]
-                w = 1
+                w = costs[str(closest_node)][str(node)]
+                # Uncomment for SP
+                # w = 1
                 if distance[closest_node] + w < distance[node]:
                     distance[node] = distance[closest_node] + w
                     previous[node] = closest_node
@@ -69,6 +77,7 @@ def get_path_dynamic(src, dst, first_port, final_port):
     path.append(dest)
     current_node = previous[dest]
 
+    # iterate through the previous dict until we reach the src
     while current_node is not None:
         if current_node == src:
             path.append(current_node)
@@ -82,25 +91,23 @@ def get_path_dynamic(src, dst, first_port, final_port):
 
     if src == dest:
         path = [src]
-        distance[src] = adjacency[src][dest]
-
-    print(json.dumps(costs))
-    print("Shortest path between ", src, " and ", dst, " is: ",
-        path, " and the total distance is: ", distance[dst])
+    
+    # add to the paths dict
+    paths[(src_ip,dst_ip)] = path
+    paths[(dst_ip,src_ip)] = path
    
-    # Now add the ports
-    r = []
+    # add the ports
+    ports = []
     in_port = first_port
 
+    # zip function for parallel iteration
     for s1, s2 in zip(path[:-1], path[1:]):
         out_port = adjacency[s1][s2]
-        r.append((s1, in_port, out_port, distance[s2]))
+        ports.append((s1, in_port, out_port, distance[s2]))
         in_port = adjacency[s2][s1]
-        n_flows[str(s1)][str(s2)] = n_flows[str(s1)][str(s2)] + 1
 
-    r.append((dst, in_port, final_port, distance[dst]))
-    return r
-
+    ports.append((dst, in_port, final_port, distance[dst]))
+    return ports
 
 def minimum_distance(distance, Q):
     min = float('Inf')
@@ -112,7 +119,6 @@ def minimum_distance(distance, Q):
             node = v
     return node
 
-
 class ProjectController(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
 
@@ -122,10 +128,9 @@ class ProjectController(app_manager.RyuApp):
         self.topology_api_app = self
         self.datapaths = {}
         self.monitor_thread = hub.spawn(self._monitor)
-        global bw
-        global costs
-        global n_flows
+        global bw, costs, n_flows
 
+        # initialize the nested dicts with the switches and links data
         try:
             fin = open("topology_info.txt", "r")
             for line in fin:
@@ -135,8 +140,8 @@ class ProjectController(app_manager.RyuApp):
                     bw[str(a[1])][str(a[0])] = int(a[2])
                     costs[str(a[0])][str(a[1])] = 0
                     costs[str(a[1])][str(a[0])] = 0
-                    n_flows[str(a[0])][str(a[1])] = int(a[4])
-                    n_flows[str(a[1])][str(a[0])] = int(a[4])      
+                    n_flows[str(a[0])][str(a[1])] = 0
+                    n_flows[str(a[1])][str(a[0])] = 0  
 
             fin.close()
 
@@ -162,7 +167,7 @@ class ProjectController(app_manager.RyuApp):
             for dp in self.datapaths.values():
                 self._request_stats(dp)
             hub.sleep(3)
-
+    
     def _request_stats(self, datapath):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
@@ -173,10 +178,52 @@ class ProjectController(app_manager.RyuApp):
 
     @set_ev_cls(ofp_event.EventOFPFlowStatsReply, MAIN_DISPATCHER)
     def _flow_stats_reply_handler(self, ev):
+        global ended_flows, active_paths, last_active_paths_len, requested_flows
+        
         body = ev.msg.body
-        #print("----------------\nFLOW ADDED: ", body)
-        #print("\n", len(body), "\n")
-
+        temp_paths = {}
+        
+        for item in body:
+            eth_src = item.match.get('eth_src')
+            eth_dst = item.match.get('eth_dst')
+            # filter registered flows by their src and dst, which need to be defined
+            if eth_src != None and eth_dst != None:
+                # for the pair src, dst, fetch the saved path
+                temp_paths[(eth_src, eth_dst)] = paths.get((eth_src, eth_dst))
+        
+        # if both (src, dst) and (dst, src) were filtered, the flow is running
+        active_paths = {k:v for (k,v) in temp_paths.items() if k in temp_paths.keys() and k[::-1] in temp_paths.keys()}
+        
+        # save the remaining entries, that represent finished flows (iperfs)
+        diff = {k : temp_paths[k] for k in set(temp_paths) - set(active_paths)}
+        
+        # to prevent misreads (len(active_paths) - len(diff))
+        # if there are any active flows (len(active_paths) > 0)
+        # protection against false reads (len(active_paths) >= last_active_paths_len/2)
+        # when all the iperfs started are terminated (requested_flows == len(diff))
+        if ((len(active_paths) - len(diff)) != 0 and len(active_paths) > 0 and len(active_paths) >= last_active_paths_len/2) \
+            or requested_flows == len(diff):
+            
+            # everytime the active flows reaches a new maximum
+            if len(active_paths)/2 + len(diff) > requested_flows:
+                # /2 because both (src, dst) and (dst, src) exist
+                requested_flows = int(len(active_paths)/2)
+                
+            # if there are finished flows, the number of active paths is lower than it was and the filtering was done correctly
+            if len(diff) > 0 and len(active_paths) < last_active_paths_len and len(active_paths) % 2 == 0:
+                paths.pop(list(diff.keys())[0], None)
+                p_dict.pop(list(diff.keys())[0], None)
+                paths.pop(list(diff.keys())[0][::-1], None)
+                p_dict.pop(list(diff.keys())[0][::-1], None)
+            
+            # update the number of flows
+            self.update_flows()    
+            # remember the number of active paths  
+            last_active_paths_len = len(active_paths)
+            
+        print("ACTIVE PATHS:", active_paths)      
+        print("N FLOWS:", json.dumps(n_flows))  
+            
     @set_ev_cls(ofp_event.EventOFPPortStatsReply, MAIN_DISPATCHER)
     def _port_stats_reply_handler(self, ev):
         global byte, clock, bw_used, bw_available, costs
@@ -185,87 +232,68 @@ class ProjectController(app_manager.RyuApp):
         dpid = ev.msg.datapath.id
 
         for stat in sorted(body, key=attrgetter('port_no')):
-            for p in _switches:
-                if adjacency[dpid][p] == stat.port_no:
-                    if byte[dpid][p] != None:
-                        if byte[dpid][p] > 0:
-                            bw_used[dpid][p] = (stat.tx_bytes - byte[dpid][p]) * \
-                                8.0 / (time.time()-clock[dpid][p]) / 1000
+            for sw in _switches:
+                if adjacency[dpid][sw] == stat.port_no:
+                    if byte[dpid][sw] != None:
+                        if byte[dpid][sw] > 0:
+                            bw_used[dpid][sw] = (stat.tx_bytes - byte[dpid][sw]) * 8.0 \
+                                / (time.time() - clock[dpid][sw]) / 1000
 
-                            bw_available[str(dpid)][str(p)] = int(
-                                bw[str(dpid)][str(p)]) * 1024.0 - bw_used[dpid][p]
+                            bw_available[str(dpid)][str(sw)] = int(bw[str(dpid)][str(sw)]) \
+                                * 1024.0 - bw_used[dpid][sw]
                             
-                            costs[str(dpid)][str(p)] = 1/bw_available[str(dpid)][str(p)]
+                            # Uncomment for DSP
+                            # costs[str(dpid)][str(p)] = 1/bw_available[str(dpid)][str(p)]
                             
-                            print("WEIGHTS UPDATED ", json.dumps(bw_available))
-
-                    byte[dpid][p] = stat.tx_bytes
-                    clock[dpid][p] = time.time()
-                    #print("WEIGHTS UPDATED ", clock[dpid][p])
-            
-        # print("--------------------")        
-        # print(json.dumps(bw))
-        # print("--------------------")
-        # print(json.dumps(bw_available))
-        # print("--------------------")    
-        # print(json.dumps(costs))
-        # print("--------------------")    
-
+                            if n_flows[str(dpid)][str(sw)] == 0 and n_flows[str(sw)][str(dpid)] == 0:
+                                costs[str(dpid)][str(sw)] = 1/bw_available[str(dpid)][str(sw)]
+                            else:
+                                costs[str(dpid)][str(sw)] = (n_flows[str(dpid)][str(sw)]/2 + \
+                                    n_flows[str(sw)][str(dpid)]/2)/bw_available[str(dpid)][str(sw)]
+                            
+                            print("BANDWIDTH: ", json.dumps(bw_available))
+                            
+                    byte[dpid][sw] = stat.tx_bytes
+                    clock[dpid][sw] = time.time()
+                    
+    def update_flows(self):
+        global n_flows, active_paths
+        
+        for k, v in n_flows.items():
+            for k1 in v.keys():
+                n_flows[str(k)][str(k1)] = 0
+        for item in active_paths:
+            if active_paths[item] != None:
+                for pos in range(len(active_paths[item])-1):
+                    n_flows[str(active_paths[item][pos])][str(active_paths[item][pos+1])] = \
+                        n_flows[str(active_paths[item][pos])][str(active_paths[item][pos+1])] + 1   
 
     def add_flow(self, datapath, priority, match, actions, buffer_id=None):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
-        #match = datapath.ofproto_parser.OFPMatch(in_port=in_port, eth_dst=dst)
-
-        # inst = [parser.OFPInstructionActions(
-        #     ofproto.OFPIT_APPLY_ACTIONS, actions)]
-
-        # mod = datapath.ofproto_parser.OFPFlowMod(
-        #     datapath=datapath, match=match, cookie=0,
-        #     command=ofproto.OFPFC_ADD, idle_timeout=0, hard_timeout=0,
-        #     priority=ofproto.OFP_DEFAULT_PRIORITY, instructions=inst)
-
-        # datapath.send_msg(mod)
         
         inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
                                              actions)]
-    
+        
         if buffer_id:
             mod = parser.OFPFlowMod(datapath=datapath, buffer_id=buffer_id,
-                                    priority=priority, match=match,
+                                    priority=priority, match=match, idle_timeout=3,
                                     instructions=inst)
         else:
-            mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
+            mod = parser.OFPFlowMod(datapath=datapath, priority=priority, idle_timeout=3,
                                     match=match, instructions=inst)
-        # Once the message is created,
-        # Ryu will take care of the rest to ensure the message is properly encoded and sent to the switch.
+            
         datapath.send_msg(mod)
 
     def install_path(self, p, ev, src_mac, dst_mac):
         msg = ev.msg
         datapath = msg.datapath
-        ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
 
         for sw, in_port, out_port, cost in p:
-            #print(src_mac, "->", dst_mac, "via ", sw,
-            #     " in_port=", in_port, " out_port=", out_port, " cost=", cost)
-
-            match = parser.OFPMatch(
-                in_port=in_port, eth_src=src_mac, eth_dst=dst_mac)
-            
+            match = parser.OFPMatch(in_port=in_port, eth_src=src_mac, eth_dst=dst_mac)
             actions = [parser.OFPActionOutput(out_port)]
-            
             datapath = datapath_list[sw]
-            
-            # inst = [parser.OFPInstructionActions(
-            #     ofproto.OFPIT_APPLY_ACTIONS, actions)]
-            
-            # mod = datapath.ofproto_parser.OFPFlowMod(
-            #     datapath=datapath, match=match, idle_timeout=0, hard_timeout=0,
-            #     priority=1, instructions=inst)
-
-            # datapath.send_msg(mod)
             self.add_flow(datapath, 1, match, actions)
 
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
@@ -274,31 +302,22 @@ class ProjectController(app_manager.RyuApp):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
         match = parser.OFPMatch()
+        
+        # installing a table-miss flow entry in the switch
 
         actions = [parser.OFPActionOutput(
             ofproto.OFPP_CONTROLLER, ofproto.OFPCML_NO_BUFFER)]
-
-        # inst = [parser.OFPInstructionActions(
-        #     ofproto.OFPIT_APPLY_ACTIONS, actions)]
-
-        # mod = datapath.ofproto_parser.OFPFlowMod(
-        #     datapath=datapath, match=match, cookie=0,
-        #     command=ofproto.OFPFC_ADD, idle_timeout=0, hard_timeout=0,
-        #     priority=0, instructions=inst)
-
-        # datapath.send_msg(mod)
         
         self.add_flow(datapath, 0, match, actions)
 
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev):
-        global target_srcmac, target_dstmac
-
         msg = ev.msg
         datapath = msg.datapath
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
         in_port = msg.match['in_port']
+        
         pkt = packet.Packet(msg.data)
         eth = pkt.get_protocol(ethernet.ethernet)
         
@@ -307,42 +326,43 @@ class ProjectController(app_manager.RyuApp):
 
         dst = eth.dst
         src = eth.src
-        
-        #dpid = format(datapath.id, "d").zfill(16)
-        
-        dpid = datapath.id
 
+        dpid = datapath.id
+        
+        # initialize mac_to_port for the switch
         self.mac_to_port.setdefault(dpid, {})
+        
+        # save the mac address to avoid flooding (added)
+        self.mac_to_port[dpid][src] = in_port
 
         if src not in mymac.keys():
             mymac[src] = (dpid, in_port)
-            
-        #self.mac_to_port[dpid][src] = in_port
-            
-        if dst in mymac.keys():
-            #if (src == target_srcmac and dst == target_dstmac) or (dst == target_srcmac and src == target_dstmac):
-            p = get_path_dynamic(mymac[src][0], mymac[dst][0],
-                        mymac[src][1], mymac[dst][1])
-
+        
+        # mymac.keys()
+        if dst in self.mac_to_port[dpid]:
+            if p_dict.get((src, dst)) != None:
+                p = p_dict.get((src, dst))
+            else:
+                p = get_path_dynamic(mymac[src][0], mymac[dst][0], mymac[src][1], mymac[dst][1], src, dst)
+                p_dict[(src, dst)] = p
             self.install_path(p, ev, src, dst)
-            out_port = p[0][2]
-            # else:
-            #     out_port = ofproto.OFPP_FLOOD
-            
+            out_port = self.mac_to_port[dpid][dst]
+            # out_port = p[0][2]
         else:
+            # if the dst mac address wasn't received earlier
             out_port = ofproto.OFPP_FLOOD
 
         actions = [parser.OFPActionOutput(out_port)]
         
-        if out_port != ofproto.OFPP_FLOOD:
-            match = parser.OFPMatch(
-				in_port=in_port, eth_src=src, eth_dst=dst)
+        # if out_port != ofproto.OFPP_FLOOD:
+        #     match = parser.OFPMatch(in_port=in_port, eth_src=src, eth_dst=dst)
             
         data = None
         
         if msg.buffer_id == ofproto.OFP_NO_BUFFER:
             data = msg.data
             
+        # send packetOut message
         if out_port == ofproto.OFPP_FLOOD:
             while len(actions) > 0:
                 actions.pop()
@@ -352,8 +372,7 @@ class ProjectController(app_manager.RyuApp):
                 
             out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
 									in_port=in_port, actions=actions, data=data)
-            datapath.send_msg(out)
-            
+            datapath.send_msg(out)   
         else:
             out = parser.OFPPacketOut(
 				datapath=datapath, buffer_id=msg.buffer_id, in_port=in_port,
@@ -373,19 +392,21 @@ class ProjectController(app_manager.RyuApp):
     def get_topology_data(self, ev):
         global _switches, adjacency, datapath_list
 
+        # get all switches
         switch_list = get_switch(self.topology_api_app, None)
+        
+        # get switches id's
         _switches = [switch.dp.id for switch in switch_list]
+        
+        # setting datapath_dict to {sw_id: sw_datapath}
+        datapath_list = dict([(switch.dp.id, switch.dp) for switch in switch_list])
 
-        for switch in switch_list:
-            datapath_list[switch.dp.id] = switch.dp
-
-       # print("Current switches = ", _switches)
-
+        # get all links
         links_list = get_link(self.topology_api_app, None)
-        mylinks = [(link.src.dpid, link.dst.dpid, link.src.port_no,
-                    link.dst.port_no) for link in links_list]
+        
+        # create list of connections
+        _links = [(link.src.dpid, link.dst.dpid, link.src.port_no, link.dst.port_no) for link in links_list]
 
-        for s1, s2, port1, port2 in mylinks:
+        for s1, s2, port1, port2 in _links:
             adjacency[s1][s2] = port1
             adjacency[s2][s1] = port2
-          # print("Connection: sw ", s1, ", port ", port1, " <---> sw", s2, ", port", port2, ", cost", costs[str(s1)][str(s2)])

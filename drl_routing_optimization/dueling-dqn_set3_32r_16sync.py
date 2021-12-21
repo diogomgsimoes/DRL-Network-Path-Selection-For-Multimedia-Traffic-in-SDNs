33d5b4f20f449ff1769bbaef0b5417d748a9c7d4
Diogo Miguel Simões <46751892+diogomgsimoes@users.noreply.github.com>
2021-12-21 10:52:50 +0000

    Update dueling-dqn_set3_32r_16sync.py

--- drl_routing_optimization/dueling-dqn_set3_32r_16sync.py
+++ drl_routing_optimization/dueling-dqn_set3_32r_16sync.py
@@ -84,9 +84,8 @@ class DRLEngine():
                                     path[i] = int(path[i])
 
     def make_reservation(self, path_id):
-        random.shuffle(self.host_pairs)
         pair = self.host_pairs.pop(0)
-        path = self.paths[(pair[0], pair[1])][path_id][1:-1]
+        path = self.paths[(pair[0], pair[1])][path_id]
         request_bw = self.requests_bw[random.randint(0, 4)]
 
         for s1, s2 in zip(path[:-1], path[1:]):
@@ -99,6 +98,15 @@ class DRLEngine():
                 if self.current_link_bw[(str(s2), str(s1))] == 0:
                     self.current_link_bw[(str(s2), str(s1))] = 1
 
+    def get_percentage(self, src, dst, bw):
+        if self.link_bw_capacity.get((src, dst)):
+            return (bw / self.link_bw_capacity.get((src, dst))) * 100
+        else:
+            return None
+
+    def get_state_helper(self):
+        return self.state_helper
+
     def build_state(self):
         state = np.empty((NUMBER_OF_HOSTS, NUMBER_OF_HOSTS, NUMBER_OF_PATHS, 1), dtype=object)
 
@@ -106,31 +114,30 @@ class DRLEngine():
             h_src = "H{}".format(src)
             for dst in range(1, NUMBER_OF_HOSTS + 1):
                 h_dst = "H{}".format(dst)
-                min_value = float('Inf')
                 cnt = 0
                 if len(self.paths[(h_src, h_dst)]) == 1:
-                    if self.paths[(h_src, h_dst)] == []:
+                    if not self.paths[(h_src, h_dst)]:
                         for idx in range(NUMBER_OF_PATHS):
-                            state[src - 1, dst - 1, idx] = 1
+                            state[src - 1, dst - 1, idx] = -1
                     else:
                         state[src - 1, dst - 1, 0] = 100
                         for idx in range(1, NUMBER_OF_PATHS):
-                            state[src - 1, dst - 1, idx] = 1
+                            state[src - 1, dst - 1, idx] = -1
                 else:
                     for path in self.paths[(h_src, h_dst)]:
-                        path = path[1:-1]
+                        min_value = float('Inf')
                         for s1, s2 in zip(path[:-1], path[1:]):
-                            stats = self.current_link_bw.get((str(s1), str(s2)))
+                            stats = self.current_link_bw.get((s1, s2))
                             if stats:
                                 if float(stats) < float(min_value):
-                                    min_value = self.current_link_bw[(str(s1), str(s2))]
+                                    min_value = self.current_link_bw[(s1, s2)]
+                                    self.state_helper[str(src) + "_" + str(dst) + "_" + str(cnt)] = str(s1) + "_" + str(s2)
 
                         state[src - 1, dst - 1, cnt] = float(min_value)
                         cnt += 1
 
                     for idx in range(len(self.paths[(h_src, h_dst)]), NUMBER_OF_PATHS):
-                        state[src - 1, dst - 1, idx] = 1
-
+                        state[src - 1, dst - 1, idx] = -1
         return state
 
     def reset(self):
@@ -163,7 +170,7 @@ class RoutingEnv(Env):
         self.action_space = Discrete(NUMBER_OF_PATHS)
         self.state = np.full((NUMBER_OF_HOSTS, NUMBER_OF_HOSTS, NUMBER_OF_PATHS, 1), 100, dtype=np.float32)
 
-    def step(self, action):
+   def step(self, action):
         self.engine.make_reservation(action)
         self.requests += 1
 
@@ -174,23 +181,28 @@ class RoutingEnv(Env):
             for dst in range(NUMBER_OF_HOSTS):
                 for path_number in range(NUMBER_OF_PATHS):
                     bw = self.state[src, dst, path_number]
-                    if bw is not None:
-                        if bw > 75:
-                            reward += 50
-                        elif bw > 50:
-                            reward += 30
-                        elif bw > 25:
-                            pass
-                        elif bw > 0:
-                            reward -= 10
-                        else:
-                            reward -= 100
+                    link = self.engine.get_state_helper().get(
+                        str(src + 1) + "_" + str(dst + 1) + "_" + str(path_number))
+                    if link:
+                        ex_link = link.split("_")
+                        bw_percentage = self.engine.get_percentage(ex_link[0], ex_link[1], bw[0])
+                        if bw_percentage is not None:
+                            if bw_percentage > 75:
+                                reward += 50
+                            elif bw_percentage > 50:
+                                reward += 30
+                            elif bw_percentage > 25:
+                                pass
+                            elif bw_percentage > 0:
+                                reward -= 10
+                            else:
+                                reward -= 100
 
         if self.requests == self.max_requests:
             self.done = True
 
-        return self.state, (reward / REWARD_SCALE) / self.max_requests, self.done, {}
-
+        return self.state, (reward / REWARD_SCALE), self.done, {}
+    
     def render(self):
         pass
 
@@ -199,14 +211,14 @@ class RoutingEnv(Env):
 
     def reset(self):
         self.done = False
-        self.state = np.full((NUMBER_OF_HOSTS, NUMBER_OF_HOSTS, NUMBER_OF_PATHS, 1), 100, dtype=np.float32)
+        self.engine.reset()
+        self.state = self.engine.build_state()
         self.requests = 0
-        self.max_requests = random.randint(1, 32)
+
         r = int(np.random.normal(24, 8))
         while r > 32 or r < 1:
             r = int(np.random.normal(24, 8))
         self.max_requests = r
-        self.engine.reset()
 
         return self.state
 
